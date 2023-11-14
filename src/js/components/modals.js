25130635aa1ba47125f4d5cd6a627b76879d40b0
Maksim Sukhov <silveraven@yandex.ru>
2023-11-14 20:43:25 +0200

    fixed errors

--- src/js/components/modals.js
+++ src/js/components/modals.js
@@ -1,11 +1,11 @@
 let btns = document.querySelectorAll('.btn-modal');
 const modals = document.querySelectorAll('.modal');
 const modalsOverlay = document.querySelector('.modals__overlay');
-const modalsBlock = document.querySelectorAll('.modals');
+const fixBlocks = document.querySelectorAll('.fix-block');
 const closeBtn = document.querySelectorAll('.modal__btn-close');
 
 
-let fixBlocks = document.querySelectorAll('.fix-block');
+
 
 
 let disableScroll = function () {
@@ -31,34 +31,35 @@ let enableScroll = function () {
 
 btns.forEach((el) => {
   el.addEventListener('click', (e) => {
-    let path = e.currentTarget.getAttribute('data-path')
+    let path = e.currentTarget.getAttribute('data-path');
     disableScroll();
     modals.forEach((el) => {
       el.classList.remove('modal--visible');
     })
-    document.querySelector(`[data-target="${path}"]`).classList.add('modal--visible')
-    modalsOverlay.classList.add('modals__overlay--visible')
-    document.body.classList.add('stop-scroll')
+    document.querySelector(`[data-target="${path}"]`).classList.add('modal--visible');
+    modalsOverlay.classList.add('modals__overlay--visible');
+    document.body.classList.add('stop-scroll');
   })
 })
 
 modalsOverlay.addEventListener('click', (e) => {
-  enableScroll();
   if(e.target === modalsOverlay) {
-    modalsOverlay.classList.remove('modals__overlay--visible')
+    modalsOverlay.classList.remove('modals__overlay--visible');
     modals.forEach((el) => {
-      el.classList.remove('modal--visible')
-      document.body.classList.remove('stop-scroll')
+      enableScroll();
+      el.classList.remove('modal--visible');
+      document.body.classList.remove('stop-scroll');
     })
   }
 });
 
 closeBtn.forEach((el) => {
   el.addEventListener('click', () => {
-    modalsOverlay.classList.remove('modals__overlay--visible')
+    enableScroll();
+    modalsOverlay.classList.remove('modals__overlay--visible');
     modals.forEach((el) => {
-      el.classList.remove('modal--visible')
-      document.body.classList.remove('stop-scroll')
+      el.classList.remove('modal--visible');
+      document.body.classList.remove('stop-scroll');
     })
   })
 })
