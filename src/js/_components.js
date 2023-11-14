29f4969b8c9be3bfca80cad3474854f9f09e86e9
Maksim Sukhov <silveraven@yandex.ru>
2023-11-14 23:54:54 +0200

    fixed errors

--- src/js/_components.js
+++ src/js/_components.js
@@ -2,3 +2,4 @@ import './components/swiper';
 import './components/validation';
 import './components/modals';
 import './components/theme';
+import './components/smothScroll';
--- src/js/components/modals.js
+++ src/js/components/modals.js
@@ -1,32 +1,11 @@
+import {disableScroll} from '../functions/disable-scroll';
+import {enableScroll} from '../functions/enable-scroll';
+
 let btns = document.querySelectorAll('.btn-modal');
 const modals = document.querySelectorAll('.modal');
 const modalsOverlay = document.querySelector('.modals__overlay');
-const fixBlocks = document.querySelectorAll('.fix-block');
 const closeBtn = document.querySelectorAll('.modal__btn-close');
 
-
-
-
-
-let disableScroll = function () {
-  let paddingOffset = window.innerWidth - document.body.offsetWidth + 'px';
-	let pagePosition = window.scrollY;
-  fixBlocks.forEach((el) => {
-		el.style.paddingRight = paddingOffset;
-	});
-	document.body.style.paddingRight = paddingOffset;
-	document.body.classList.add('stop-scroll');
-	document.body.dataset.position = pagePosition;
-	document.body.style.top = -pagePosition + 'px';
-}
-
-let enableScroll = function () {
-  document.body.classList.remove('stop-scroll');
-  fixBlocks.forEach((el) => {
-    el.style.paddingRight = '0px';
-  });
-  document.body.style.paddingRight = '0px';
-}
 
 
 btns.forEach((el) => {
